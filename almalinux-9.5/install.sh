ee9ccdc5eb51de40171df344ccc10630adfa75d0
Bozhidar Slaveykov <bobicloudvision@gmail.com>
2025-03-09 03:32:02 +0200

    update

--- almalinux-9.5/install.sh
+++ almalinux-9.5/install.sh
@@ -114,14 +114,15 @@ cd /usr/local/bolt/web
 chmod -R o+w /usr/local/bolt/web/storage/
 chmod -R o+w /usr/local/bolt/web/bootstrap/cache/
 
-rm -rf /usr/local/bolt/nginx/conf/nginx.conf
-cp /usr/local/bolt/web/server/nginx/nginx.conf /usr/local/bolt/nginx/conf/nginx.conf
+rm -f /usr/local/bolt/php/lib/php.ini
+ln -s /usr/local/bolt/web/server/php/php.ini /usr/local/bolt/php/lib/php.ini
 
 rm -rf /usr/local/bolt/php/etc/php-fpm.conf
-cp /usr/local/bolt/web/server/php/php-fpm.conf /usr/local/bolt/php/etc/php-fpm.conf
+ln -s /usr/local/bolt/web/server/php/php-fpm.conf /usr/local/bolt/php/etc/php-fpm.conf
 
-rm -rf /usr/local/bolt/php/lib/php.ini
-cp /usr/local/bolt/web/server/php/php.ini /usr/local/bolt/php/lib/php.ini
+rm -rf /usr/local/bolt/nginx/conf/nginx.conf
+ln -s /usr/local/bolt/web/server/nginx/nginx.conf /usr/local/bolt/nginx/conf/nginx.conf
+
 
 service bolt start
 
